82ae59f0b0afb5dc1c2595290d4d121fe7b866fd
yuki528 <Kiplimomeli44@gmail.com>
2022-10-18 04:16:07 -0700

    tryingn

--- tils.c
+++ tils.c
@@ -3,6 +3,7 @@
 /**
  * is_printable - Evaluates if a char is printable
  * @c: Char to be evaluated.
+ *valuse
  *
  * Return: 1 if c is printable, 0 otherwise
  */
@@ -19,6 +20,7 @@ int is_printable(char c)
  * @buffer: Array of chars.
  * @i: Index at which to start appending.
  * @ascii_code: ASSCI CODE.
+ *
  * Return: Always 3
  */
 int append_hexa_code(char ascii_code, char buffer[], int i)
